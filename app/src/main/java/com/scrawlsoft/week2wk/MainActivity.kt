6bbda14401de64216c633e1416d831bd288f65c6
George Madrid <gmadrid@gmail.com>
2018-02-20 16:31:49 -0500

    Adding some random data to Firestore

--- app/src/main/java/com/scrawlsoft/week2wk/MainActivity.kt
+++ app/src/main/java/com/scrawlsoft/week2wk/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.scrawlsoft.week2wk
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : SignedInActivity() {
+    private val TAG: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +20,19 @@ class MainActivity : SignedInActivity() {
         val account = GoogleSignIn.getLastSignedInAccount(this)
         print("GEORGE")
         print(account)
+
+        Toast.makeText(this, "HI", Toast.LENGTH_LONG).show()
+
+        val db = FirebaseFirestore.getInstance()
+
+        val user = HashMap<String,String>()
+        user.put("GEORGE", "MADRID")
+        user.put("WAS", "here")
+        user.put("quux", "foosball")
+
+        db.collection("users")
+                .add(user as Map<String, Any>)
+                .addOnSuccessListener { Log.d(TAG, "SUCCESS") }
+                .addOnFailureListener { e -> Log.d(TAG, "FAILURE: $e") }
     }
 }
