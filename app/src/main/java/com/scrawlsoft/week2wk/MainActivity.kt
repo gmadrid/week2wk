65f6ac879f9d2a2f51dff77c98b4295fd2d5d7a9
George Madrid <gmadrid@gmail.com>
2018-02-22 12:20:08 -0500

    Beginning some material design. Still no items.

--- app/src/main/java/com/scrawlsoft/week2wk/MainActivity.kt
+++ app/src/main/java/com/scrawlsoft/week2wk/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.scrawlsoft.week2wk
 
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.widget.Toolbar
 import android.util.Log
-import android.widget.Button
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : SignedInActivity() {
@@ -11,36 +13,30 @@ class MainActivity : SignedInActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val actionBar = supportActionBar
+        actionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 
     override fun onStart() {
         super.onStart()
 
-        findViewById<Button>(R.id.thebutton).setOnClickListener {
+        findViewById<FloatingActionButton>(R.id.task_fab).setOnClickListener {
             val task = TaskModel(getUid(), "The task text")
 
             val db = FirebaseFirestore.getInstance()
             db.collection("users").document(getUid())
                     .collection("tasks").add(task)
-                    .addOnSuccessListener { Log.d(TAG, "SUCCESS") }
+                    .addOnSuccessListener {
+                        Snackbar.make(findViewById(R.id.main_container), "Task added", Snackbar.LENGTH_SHORT).show()
+                        Log.d(TAG, "SUCCESS")
+                    }
                     .addOnFailureListener { Log.d(TAG, "FAILURE") }
         }
-
-//        val account = GoogleSignIn.getLastSignedInAccount(this)
-
-//        Toast.makeText(this, "HI", Toast.LENGTH_LONG).show()
-
-//        val db = FirebaseFirestore.getInstance()
-
-//        val user = HashMap<String,String>()
-//        user.put("GEORGE", "MADRID")
-//        user.put("WAS", "here")
-//        user.put("quux", "foosball")
-//
-//        db.collection("users")
-//                .add(user as Map<String, Any>)
-//                .addOnSuccessListener { Log.d(TAG, "SUCCESS") }
-//                .addOnFailureListener { e -> Log.d(TAG, "FAILURE: $e") }
-
     }
 }
