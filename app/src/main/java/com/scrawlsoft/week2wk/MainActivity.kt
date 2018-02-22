9a9990f4c30e20ea446dd96a69e96692d8fce566
George Madrid <gmadrid@gmail.com>
2018-02-22 16:30:46 -0500

    Using kotlin android extensions. Also, have a scrolling list (with no real data, yet).

--- app/src/main/java/com/scrawlsoft/week2wk/MainActivity.kt
+++ app/src/main/java/com/scrawlsoft/week2wk/MainActivity.kt
@@ -1,31 +1,56 @@
 package com.scrawlsoft.week2wk
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
-import android.support.v7.widget.Toolbar
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.TextView
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : SignedInActivity() {
+    class ViewHolderr(val view: TextView) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    class Adapterr : RecyclerView.Adapter<ViewHolderr>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderr {
+            val textView = TextView(parent.context)
+            Log.d("ADAPTER", "CVH")
+            return ViewHolderr(textView)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderr, position: Int) {
+            holder.view.text = "GEORGE: $position"
+        }
+
+        override fun getItemCount(): Int = 60
+    }
+
     private val TAG: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val actionBar = supportActionBar
-        actionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
-        actionBar?.setDisplayHomeAsUpEnabled(true)
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+
+        task_recycler.adapter = Adapterr()
+        task_recycler.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onStart() {
         super.onStart()
 
-        findViewById<FloatingActionButton>(R.id.task_fab).setOnClickListener {
+        task_fab.setOnClickListener {
             val task = TaskModel(getUid(), "The task text")
 
             val db = FirebaseFirestore.getInstance()
