8396beed4f674445981f634ecd1d67bf1a504b32
George Madrid <gmadrid@gmail.com>
2018-02-23 10:59:20 -0500

    Code cleanup and reorg

--- app/src/main/java/com/scrawlsoft/week2wk/MainActivity.kt
+++ app/src/main/java/com/scrawlsoft/week2wk/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
@@ -13,31 +12,49 @@ import android.widget.TextView
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : SignedInActivity() {
-    class ViewHolderr(val view: TextView) : RecyclerView.ViewHolder(view) {
-
+fun View.showSoftKeyboardOnFocus(context: Context) {
+    this.setOnFocusChangeListener { view, hasFocus ->
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
     }
+}
 
-    class Adapterr : RecyclerView.Adapter<ViewHolderr>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderr {
+class MainActivity : SignedInActivity() {
+    private val TAG: String = this.javaClass.simpleName
+
+    class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             val textView = TextView(parent.context)
-            Log.d("ADAPTER", "CVH")
-            return ViewHolderr(textView)
+            return ViewHolder(textView)
         }
 
-        override fun onBindViewHolder(holder: ViewHolderr, position: Int) {
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             holder.view.text = "GEORGE: $position"
         }
 
         override fun getItemCount(): Int = 60
-    }
 
-    private val TAG: String = "MainActivity"
+        class ViewHolder(val view: TextView) : RecyclerView.ViewHolder(view) {
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupActionBar()
+        setupRecycler()
+        setupFab()
+        setupTaskEdit()
+    }
+
+    private fun setupRecycler() {
+        task_recycler.adapter = Adapter()
+        task_recycler.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupActionBar() {
         setSupportActionBar(toolbar)
 
         val actionBar = supportActionBar
@@ -45,10 +62,13 @@ class MainActivity : SignedInActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
             actionBar.setDisplayHomeAsUpEnabled(true)
         }
+    }
 
-        task_recycler.adapter = Adapterr()
-        task_recycler.layoutManager = LinearLayoutManager(this)
+    private fun setupFab() {
+        task_fab.setOnClickListener { showTaskEdit() }
+    }
 
+    private fun setupTaskEdit() {
         add_task_text.setOnEditorActionListener { textView, i, keyEvent ->
             val desc: String = textView.text.toString()
             textView.text = ""
@@ -61,11 +81,7 @@ class MainActivity : SignedInActivity() {
                         .collection("tasks").add(task)
                         .addOnSuccessListener {
                             Snackbar.make(main_container, "Task added: $desc", Snackbar.LENGTH_SHORT).show()
-                            task_fab.visibility = View.VISIBLE
-                            add_task_frame.visibility = View.GONE
-
-                            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                            imm.hideSoftInputFromWindow(add_task_frame.windowToken, 0)
+                            hideTaskEdit()
                         }
                         .addOnFailureListener {
                             Snackbar.make(main_container, "Failed to add task", Snackbar.LENGTH_INDEFINITE).show()
@@ -73,32 +89,20 @@ class MainActivity : SignedInActivity() {
             }
             true
         }
-        add_task_text.setOnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-            }
-        }
+        add_task_text.showSoftKeyboardOnFocus(this)
     }
 
-    override fun onStart() {
-        super.onStart()
+    private fun showTaskEdit() {
+        add_task_frame.visibility = View.VISIBLE
+        add_task_text.requestFocus()
+        task_fab.visibility = View.GONE
+    }
 
-        task_fab.setOnClickListener {
-            add_task_frame.visibility = View.VISIBLE
-            add_task_text.requestFocus()
-            task_fab.visibility = View.GONE
+    private fun hideTaskEdit() {
+        task_fab.visibility = View.VISIBLE
+        add_task_frame.visibility = View.GONE
 
-//            val task = TaskModel(getUid(), "The task text")
-//
-//            val db = FirebaseFirestore.getInstance()
-//            db.collection("users").document(getUid())
-//                    .collection("tasks").add(task)
-//                    .addOnSuccessListener {
-//                        Snackbar.make(findViewById(R.id.main_container), "Task added", Snackbar.LENGTH_SHORT).show()
-//                        Log.d(TAG, "SUCCESS")
-//                    }
-//                    .addOnFailureListener { Log.d(TAG, "FAILURE") }
-        }
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(add_task_frame.windowToken, 0)
     }
 }
